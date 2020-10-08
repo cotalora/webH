6ee1f152025755618173de9f596f08e277da7093
cotalora <67478427+cotalora@users.noreply.github.com>
2020-10-08 12:05:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ restService.post("/echo", function(req, res) {
 });
 
 
-restService.listen(port, function() {
+restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
